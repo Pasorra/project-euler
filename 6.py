7f61851dfe0c54266bb56b208bead8b284c377fc
Pasorra <mancuresed@gmail.com>
2023-07-13 20:19:36 +0300

    problem 6 complete

---
+++ 6.py
@@ -0,0 +1,7 @@
+n = 100
+sum_of_the_squares = n * (n + 1) * (2*n + 1) / 6
+square_of_the_sum = (n * (n + 1) / 2) ** 2
+print(sum_of_the_squares)
+print(square_of_the_sum)
+
+print(square_of_the_sum - sum_of_the_squares )
