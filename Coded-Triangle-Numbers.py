d1e937a140cab5190f2539bffc2b4aed43c0e0ca
Pasorra <mancuresed@gmail.com>
2023-07-15 04:01:16 +0300

    Coded Triangle Numbers complete

---
+++ Coded-Triangle-Numbers.py
@@ -0,0 +1,26 @@
+with open("words.txt", "r") as f:
+    words = [word.strip("\"") for word in f.readline().split(",")]
+
+memo = {}
+
+
+def calculate(num, n, memo):
+    x = 0
+    while num > x:
+        x = (n * (n + 1)) // 2
+        memo[n] = x
+        n += 1
+    return n
+
+
+count = 0
+last_n = 1
+for word in words:
+    val = 0
+    for character in word:
+        val += ord(character) - 64
+    last_n = calculate(val, last_n, memo)
+    if val in memo.values():
+        count += 1
+
+print(count)
